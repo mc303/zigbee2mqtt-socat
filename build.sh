aab3d806fe6a324874e991f573adbbc620c5a0d5
mc303 <daniel@buonocore.nl>
2020-03-12 21:14:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker build --no-cache -t mc303/zigbee2mqtt-socat-arm64v8 .
+docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t mc303/zigbee2mqtt-socat --push .
+
